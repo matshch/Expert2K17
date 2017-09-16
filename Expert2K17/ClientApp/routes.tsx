ecff5c74b66ef182632f03c037145a6e548e7e1b
hexagramg <hexagramg@gmail.com>
2017-09-16 21:15:24 +0300

    Бля, зачем

--- Expert2K17/ClientApp/routes.tsx
+++ Expert2K17/ClientApp/routes.tsx
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
 import FetchData from './components/FetchData';
-import Counter from './components/Counter';
+import TestCreater from './components/CreateTest';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
+    <Route path='/counter' component={ TestCreater } />
     <Route path='/fetchdata/:startDateIndex?' component={ FetchData } />
 </Layout>;
