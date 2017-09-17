79082f4949233a0ec86cf81bdbceff4b0d1513a2
Artem Leshchev <matshch@gmail.com>
2017-09-17 03:37:09 +0300

    Подрубаем шрифты

--- Expert2K17/webpack.config.js
+++ Expert2K17/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.(png|jpg|jpeg|gif|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
         plugins: [
