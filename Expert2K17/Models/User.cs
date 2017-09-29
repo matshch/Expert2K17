0587d47d7e1704fc9f19ed5d5eb04babccad80dc
Artem Leshchev <matshch@gmail.com>
2017-09-29 13:15:06 +0300

    Add User name/surname

--- Expert2K17/Models/User.cs
+++ Expert2K17/Models/User.cs
@@ -5,6 +5,10 @@ namespace Expert2K17.Models
 {
     public class User : IdentityUser
     {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+
         public virtual GroupModel Group { get; set; }
 
         [NotMapped]
