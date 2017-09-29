1abd94ce8dbfbd1d537979cfed735198f1791a48
Artem Leshchev <matshch@gmail.com>
2017-09-29 14:43:06 +0300

    It should work.

--- Expert2K17/Controllers/LoginController.cs
+++ Expert2K17/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
+using Expert2K17.Data;
 using Expert2K17.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Expert2K17.Controllers
@@ -8,11 +10,13 @@ namespace Expert2K17.Controllers
     [Route("api/[controller]")]
     public class LoginController : Controller
     {
+        private readonly Db _db;
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
 
-        public LoginController(SignInManager<User> signInManager, UserManager<User> userManager)
+        public LoginController(Db db, SignInManager<User> signInManager, UserManager<User> userManager)
         {
+            _db = db;
             _signInManager = signInManager;
             _userManager = userManager;
         }
@@ -20,10 +24,27 @@ namespace Expert2K17.Controllers
         // GET: api/login
         // Returns current user information
         [HttpGet]
-        public async Task<User> GetAsync()
+        public async Task<UserData> GetAsync()
         {
-            var user = await _userManager.GetUserAsync(User);
-            return user;
+            var userId = _userManager.GetUserId(User);
+            var user = await _db.Users.Include(e => e.Group.Year).FirstOrDefaultAsync(e => e.Id == userId);
+            if (user != null)
+            {
+                return new UserData
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Name = user.Name,
+                    Surname = user.Surname,
+                    Patronymic = user.Patronymic,
+                    Group = user.Group?.Group,
+                    Year = user.Year?.Year
+                };
+            }
+            else
+            {
+                return null;
+            }
         }
 
         // POST api/values
@@ -56,6 +77,19 @@ namespace Expert2K17.Controllers
         public async Task Delete()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public class UserData
+        {
+            public string Id { get; set; }
+            public string UserName { get; set; }
+
+            public string Surname { get; set; }
+            public string Name { get; set; }
+            public string Patronymic { get; set; }
+
+            public string Group { get; set; }
+            public string Year { get; set; }
         }
     }
 }
