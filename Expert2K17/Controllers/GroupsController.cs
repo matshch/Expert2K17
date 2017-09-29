31a611c220870c9706114a225bb8422cbf23ef49
Artem Leshchev <matshch@gmail.com>
2017-09-29 12:39:27 +0300

    Return current groups

---
+++ Expert2K17/Controllers/GroupsController.cs
@@ -0,0 +1,31 @@
+﻿using Expert2K17.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Expert2K17.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupsController : Controller
+    {
+        public readonly Db _db;
+
+        public GroupsController(Db db)
+        {
+            _db = db;
+        }
+
+        // GET: api/groups
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var years = from y in _db.Years
+                        select new
+                        {
+                            y.Year,
+                            Groups = y.Groups.Select(e => e.Group)
+                        };
+            return years.ToList();
+        }
+    }
+}
