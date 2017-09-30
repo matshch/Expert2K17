c973b5d576d00acedfc7e2ebde02fed0a2ba386d
MaxAvatar <maxavatar007@gmail.com>
2017-09-30 18:13:42 +0300

    commit

--- Expert2K17/ClientApp/components/Register.tsx
+++ Expert2K17/ClientApp/components/Register.tsx
@@ -3,15 +3,15 @@ import { Card, CardBlock, ButtonGroup, Button, Form, FormGroup, Label, Input, Fo
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
 import { ApplicationState } from '../store';
-import * as LoginStore from '../store/Login';
+import * as RegisterStore from '../store/Register';
 import '../css/cards.css';
 
-type LoginProps =
-    LoginStore.LoginState
-    & typeof LoginStore.actionCreators
+type RegisterProps =
+    RegisterStore.RegisterState
+    & typeof RegisterStore.actionCreators
     & RouteComponentProps<{}>;
 
-export class Login extends React.Component<LoginProps, { name: string, surname: string, patronymic: string, password: string }> {
+export class Register extends React.Component<RegisterProps, { name: string, surname: string, patronymic: string, password: string }> {
     constructor() {
         super();
         this.state = {
@@ -38,14 +38,20 @@ export class Login extends React.Component<LoginProps, { name: string, surname: 
 
     render() {
         return (
-            <div className="lg-card login ">
+            <div className="lg-card register">
                 <Card>
                     <Form>
                         <FormGroup row>
                             <Label for="exampleEmail">Логин</Label>
-                            <Input size="xs" type="text" name="login" id="Login" placeholder="введите логин" />
+                            <Input size="xs" type="text" placeholder="введите логин" />
+                            <Label for="exampleEmail">Имя</Label>
+                            <Input size="xs" type="text" placeholder="введите имя" />
+                            <Label for="exampleEmail">Фамилия</Label>
+                            <Input size="xs" type="text" placeholder="введите фамилию" />
                             <Label for="examplePassword">Пароль</Label>
-                            <Input size="xs" type="password" name="password" id="Password" placeholder="введите пароль" />
+                            <Input size="xs" type="password" placeholder="введите пароль" />
+                            <Label for="examplePassword">Повторите пароль</Label>
+                            <Input size="xs" type="password" placeholder="повторно введите пароль" />
                             <ButtonGroup className="btn-block">
                                 <Button block>Войти</Button>
                                 <Button color="primary">Зарегистрироваться</Button>
@@ -58,6 +64,6 @@ export class Login extends React.Component<LoginProps, { name: string, surname: 
 }
 
 export default connect(
-    (state: ApplicationState) => state.login,
-    LoginStore.actionCreators
-)(Login);
+    (state: ApplicationState) => state.register,
+    RegisterStore.actionCreators
+)(Register);
