94a4cc373ee6f22d0afac822e0f510c3b0d244fe
Artem Leshchev <matshch@gmail.com>
2017-10-09 21:36:05 +0300

    Allow using roles name in attributes

--- Expert2K17/Data/Roles.cs
+++ Expert2K17/Data/Roles.cs
@@ -9,6 +9,6 @@ namespace Expert2K17.Data
 
     public static class Role
     {
-        public static readonly string Admin = Enum.GetName(typeof(Roles), Roles.Admin);
+        public const string Admin = "Admin";
     }
 }
