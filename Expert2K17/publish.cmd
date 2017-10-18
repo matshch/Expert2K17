28e18e9fd48823ce945a9baa0abc9df4bdde48a5
Artem Leshchev <matshch@gmail.com>
2017-10-18 14:32:23 +0300

    New domain!

--- Expert2K17/publish.cmd
+++ Expert2K17/publish.cmd
@@ -1,8 +1,8 @@
 dotnet publish -c Release -r ubuntu-x64
 cd bin\Release\netcoreapp2.0\ubuntu-x64\publish
 bash -c "rsync -avz --progress --exclude connections.json --exclude wwwroot/covers --exclude wwwroot/systems --exclude wwwroot/userpics --delete * maxavatar.no-ip.org:/opt/Expert2k17/"
-bash -c "ssh maxavatar.no-ip.org mkdir -p /opt/Expert2k17/wwwroot/covers"
-bash -c "ssh maxavatar.no-ip.org mkdir -p /opt/Expert2k17/wwwroot/systems"
-bash -c "ssh maxavatar.no-ip.org mkdir -p /opt/Expert2k17/wwwroot/userpics"
-bash -c "ssh maxavatar.no-ip.org sudo expert2k17-restart.sh"
+bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/covers"
+bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/systems"
+bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/userpics"
+bash -c "ssh maxis.pw sudo expert2k17-restart.sh"
 cd ..\..\..\..\..
