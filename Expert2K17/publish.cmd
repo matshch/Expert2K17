ccaa712f5935d23a36f285b6fdac2ac787974879
Artem Leshchev <matshch@gmail.com>
2017-10-19 09:45:23 +0300

    Update publish.cmd

--- Expert2K17/publish.cmd
+++ Expert2K17/publish.cmd
@@ -1,6 +1,6 @@
 dotnet publish -c Release -r ubuntu-x64
 cd bin\Release\netcoreapp2.0\ubuntu-x64\publish
-bash -c "rsync -avz --progress --exclude connections.json --exclude wwwroot/covers --exclude wwwroot/systems --exclude wwwroot/userpics --delete * maxavatar.no-ip.org:/opt/Expert2k17/"
+bash -c "rsync -avz --progress --exclude connections.json --exclude wwwroot/covers --exclude wwwroot/systems --exclude wwwroot/userpics --delete * maxis.pw:/opt/Expert2k17/"
 bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/covers"
 bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/systems"
 bash -c "ssh maxis.pw mkdir -p /opt/Expert2k17/wwwroot/userpics"
