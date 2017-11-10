cb3252fe17b56d789fb4ae9f14ad0718770b7ade
MaxAvatar <maxavatar007@gmail.com>
2017-11-10 14:15:11 +0300

    zaz

---
+++ Expert2K17/ClientApp/components/About.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import * as Spinner from 'react-spinkit';
+import { connect } from 'react-redux';
+
+export default class About extends React.Component<HomeProps, {}> {
+    render() {
+    	<div>test</div>);
+    }
+}
